80a8148e390214bc3a909c7c61a9d00d44c8e69f
irvaine <irvainenunes.cs@gmail.com>
2025-07-29 16:37:39 -0300

    Primeiro commit do projeto

---
+++ src/main/java/org/example/Funcionario.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class Funcionario {
+    private String nome;
+    private float salario;
+    private String cpf;
+
+    public Funcionario() {
+        nome = "";
+        salario = 0;
+        cpf = "";
+    }
+
+    public Funcionario(String nome, float salario, String cpf) {
+        this.nome = nome;
+        this.salario = salario;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void apresentar() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Salário: " + salario);
+        System.out.println("Cpf: " + cpf);
+    }
+
+}
