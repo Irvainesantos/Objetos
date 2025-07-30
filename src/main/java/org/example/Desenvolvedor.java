35a51f0e243ab411332c35b3fd2de953646eb106
irvaine <irvainenunes.cs@gmail.com>
2025-07-30 17:14:24 -0300

    Herança na classe Desenvolvedor.

--- src/main/java/org/example/Desenvolvedor.java
+++ src/main/java/org/example/Desenvolvedor.java
@@ -1,6 +1,43 @@
 package org.example;
 
-public class Desenvolvedor {
-    String linguagem;
-    String frameWork;
+public class Desenvolvedor extends Funcionario {
+    private String linguagem;
+    private String frameWork;
+
+    public Desenvolvedor() {
+        linguagem = "";
+        frameWork = "";
+    }
+
+    public Desenvolvedor(String nome, float salario, String cpf, String linguagem, String frameWork) {
+        super(nome, salario, cpf);//Classe generica Classe pai - // Class Funcionario.
+        this.frameWork = frameWork;
+        this.linguagem = linguagem;
+    }
+
+    public String getLinguagem() {
+        return linguagem;
+    }
+
+    public void setLinguagem(String linguagem) {
+        this.linguagem = linguagem;
+    }
+
+    public String getFrameWork() {
+        return frameWork;
+    }
+
+    public void setFrameWork(String frameWork) {
+        this.frameWork = frameWork;
+    }
+
+    //Polimorfismo-Sobreescrita.
+    @Override
+    public void apresentar() {
+        super.apresentar();
+        System.out.println("Linguagem : " + linguagem);
+        System.out.println("Framework : " + frameWork);
+    }
+
+    //Polimorfismo-Sobrecarga (Overload).
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,10 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("gabriel");
-        funcionario.setCpf("514.767.646-74");
-        funcionario.setSalario(3900.0f);
-        funcionario.apresentar();
-
+        Desenvolvedor Desenvolvedor = new Desenvolvedor("Gabriel", 5050.50f,
+                "431.555.456-13", "Java", "Spring Boot");
+        Desenvolvedor.apresentar();
     }
 }
